93771d590797c18fca1fd94cb7cb902dd25350a0
Rohit Singh <singh385536@gmail.com>
2025-09-14 22:31:21 +0530

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Rohit Singh</h1>
+        <p className={styles.description}>
+          I’m a full-stack developer familiar with React and Node.js. I enjoy building projects and learning new things. Reach out if you’d like to know more!
+        </p>
+        <a href="#contact" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
